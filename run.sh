fae016ee2829a045a749b46d035cb11d4e09bbcd
iamlooper <iamloooper@gmail.com>
2024-01-19 18:33:02 +0500

    refactor: use venv

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
-#!/bin/bash 
+#!/bin/bash
+
+# Start venv.
+if [ -d "$PWD/venv" ]; then
+    source "$PWD/venv/bin/activate"
+fi
 
 # Start a dummy web server.
-py_code="from aiohttp import web
+if [ ! -z "$PORT" ]; then
+    py_code="from aiohttp import web
 app = web.Application()
 app.router.add_get('/', lambda _: web.Response(text='Web server is running...'))
 web.run_app(app, host='0.0.0.0', port=$PORT)"
-python -c "$py_code" &
+    python -c "$py_code" &
+fi
 
 # Start the app.
 python -m app
