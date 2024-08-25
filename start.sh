d61fc82258f4c55f502c86cb9c7752eaa24fa783
iamlooper <iamloooper@gmail.com>
2024-08-25 18:17:05 +0500

    feat: update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# Start venv.
-if [ -d "$PWD/venv" ]; then
-    source "$PWD/venv/bin/activate"
-fi
+# Install python packages
+echo "Installing python packages..."
+pip install --ignore-installed -qUr requirements.txt
 
-# Install requirements.
-pip install -r requirements.txt --no-cache-dir
-
-# Start a dummy web server.
-if [ ! -z "$PORT" ]; then
-    py_code="from aiohttp import web
-app = web.Application()
-app.router.add_get('/', lambda _: web.Response(text='Web server is running...'))
-web.run_app(app, host='0.0.0.0', port=$PORT)"
-    python -c "$py_code" &
-fi
-
-# Start the app.
+# Start the app
 python -m app
